01604e5399f88df81e4d0f6bd407397b9676adc5
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-22 15:25:02 +0200

    Initial commit

---
+++ src/main/java/FrancescaBattistini/LezioneSpring2/LezioneSpring2Application.java
@@ -0,0 +1,13 @@
+package FrancescaBattistini.LezioneSpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezioneSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LezioneSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/FrancescaBattistini/LezioneSpring2/LezioneSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package FrancescaBattistini.LezioneSpring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezioneSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
