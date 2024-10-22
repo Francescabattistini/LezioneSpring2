c96ead09db64dafb4f283f01f4bdd0699886120f
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-22 16:50:43 +0200

    classi

---
+++ src/main/java/entities/Ordine.java
@@ -0,0 +1,13 @@
+package entities;
+
+import enums.StatoOrdine;
+
+import java.util.List;
+
+public class Ordine {
+    int numeroTavolo;
+    StatoOrdine statoOrdine;
+    int numeroOrdine;
+    List<DettagliMenu> elemntiMenu;
+
+}
